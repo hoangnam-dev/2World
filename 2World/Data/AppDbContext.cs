ff9f9540f51b3b9b2f819fb35f9ca6ddc4285764
Nguyen Hoang Nam <nhnam010@gmail.com>
2024-07-14 20:56:05 +0700

    feat: add database seeding

--- 2World/Data/AppDbContext.cs
+++ 2World/Data/AppDbContext.cs
@@ -63,6 +63,9 @@ namespace _2World.Data
             .HasForeignKey(o => o.Customer_Id);
 
             modelBuilder.Entity<OrderItem>()
+            .HasKey(oi => new { oi.Order_Id, oi.Product_Id });
+
+            modelBuilder.Entity<OrderItem>()
                 .HasOne(oi => oi.Order)
                 .WithMany(o => o.OrderItems)
                 .HasForeignKey(oi => oi.Order_Id);
@@ -80,6 +83,353 @@ namespace _2World.Data
             modelBuilder.Entity<User>().ToTable("User");
             modelBuilder.Entity<Role>().ToTable("Role");
             modelBuilder.Entity<Permission>().ToTable("Permission");
+
+            modelBuilder.Entity<Role>()
+                .HasData(
+                    new Role
+                    {
+                        Id = 1,
+                        Name = "ADMIN",
+                        Created_At = DateTime.Now,
+                        Updated_At = DateTime.Now,
+                    },
+                    new Role
+                    {
+                        Id = 2,
+                        Name = "USER",
+                        Created_At = DateTime.Now,
+                        Updated_At = DateTime.Now,
+                    },
+                    new Role
+                    {
+                        Id = 3,
+                        Name = "GUEST",
+                        Created_At = DateTime.Now,
+                        Updated_At = DateTime.Now,
+                    }
+                );
+            modelBuilder.Entity<User>()
+                .HasData(
+                    new User
+                    {
+                        Id = 1,
+                        Name = "Admin",
+                        Email = "admin@gmail.com",
+                        Phone = "0987654321",
+                        Password = BCrypt.Net.BCrypt.EnhancedHashPassword("password", 13),
+                        Role_Id = 1,
+                        Created_At = DateTime.Now,
+                        Updated_At = DateTime.Now,
+                    }
+                );
+            Random random = new Random();
+            modelBuilder.Entity<Customer>()
+                .HasData(
+                    new Customer
+                    {
+                        Id = 1,
+                        Name = Faker.Name.FullName(),
+                        Email = Faker.Name.First() + "@gmail.com",
+                        Phone = Faker.Phone.Number(),
+                        Address = Faker.Address.City(),
+                        Password = BCrypt.Net.BCrypt.EnhancedHashPassword("password", 13),
+                        Created_At = DateTime.Now,
+                        Updated_At = DateTime.Now,
+                    },
+                    new Customer
+                    {
+                        Id = 2,
+                        Name = Faker.Name.FullName(),
+                        Email = Faker.Name.First() + "@gmail.com",
+                        Phone = Faker.Phone.Number(),
+                        Address = Faker.Address.City(),
+                        Password = BCrypt.Net.BCrypt.EnhancedHashPassword("password", 13),
+                        Created_At = DateTime.Now,
+                        Updated_At = DateTime.Now,
+                    },
+                    new Customer
+                    {
+                        Id = 3,
+                        Name = Faker.Name.FullName(),
+                        Email = Faker.Name.First() + "@gmail.com",
+                        Phone = Faker.Phone.Number(),
+                        Address = Faker.Address.City(),
+                        Password = BCrypt.Net.BCrypt.EnhancedHashPassword("password", 13),
+                        Created_At = DateTime.Now,
+                        Updated_At = DateTime.Now,
+                    },
+                    new Customer
+                    {
+                        Id = 4,
+                        Name = Faker.Name.FullName(),
+                        Email = Faker.Name.First() + "@gmail.com",
+                        Phone = Faker.Phone.Number(),
+                        Address = Faker.Address.City(),
+                        Password = BCrypt.Net.BCrypt.EnhancedHashPassword("password", 13),
+                        Created_At = DateTime.Now,
+                        Updated_At = DateTime.Now,
+                    },
+                    new Customer
+                    {
+                        Id = 5,
+                        Name = Faker.Name.FullName(),
+                        Email = Faker.Name.First() + "@gmail.com",
+                        Phone = Faker.Phone.Number(),
+                        Address = Faker.Address.City(),
+                        Password = BCrypt.Net.BCrypt.EnhancedHashPassword("password", 13),
+                        Created_At = DateTime.Now,
+                        Updated_At = DateTime.Now,
+                    }
+                );
+            modelBuilder.Entity<Category>()
+                .HasData(
+                    new Category
+                    {
+                        Id = 1,
+                        Name = "Books",
+                        Created_At = DateTime.Now,
+                        Updated_At = DateTime.Now,
+                    },
+                    new Category
+                    {
+                        Id = 2,
+                        Name = "Electronics",
+                        Created_At = DateTime.Now,
+                        Updated_At = DateTime.Now,
+                    },
+                    new Category
+                    {
+                        Id = 3,
+                        Name = "Clothes",
+                        Created_At = DateTime.Now,
+                        Updated_At = DateTime.Now,
+                    }
+                );
+            modelBuilder.Entity<Product>()
+                .HasData(
+                    new Product
+                    {
+                        Id = 1,
+                        Name = "Product 1",
+                        Price = 10,
+                        Quantity = 12,
+                        Category_Id = 1,
+                        Created_At = DateTime.Now,
+                        Updated_At = DateTime.Now,
+                    },
+                    new Product
+                    {
+                        Id = 2,
+                        Name = "Product 2",
+                        Price = 12,
+                        Quantity = 12,
+                        Category_Id = 2,
+                        Created_At = DateTime.Now,
+                        Updated_At = DateTime.Now,
+                    },
+                    new Product
+                    {
+                        Id = 3,
+                        Name = "Product 3",
+                        Price = 13,
+                        Quantity = 12,
+                        Category_Id = 1,
+                        Created_At = DateTime.Now,
+                        Updated_At = DateTime.Now,
+                    },
+                    new Product
+                    {
+                        Id = 4,
+                        Name = "Product 4",
+                        Price = 14,
+                        Quantity = 12,
+                        Category_Id = 3,
+                        Created_At = DateTime.Now,
+                        Updated_At = DateTime.Now,
+                    },
+                    new Product
+                    {
+                        Id = 5,
+                        Name = "Product 5",
+                        Price = 15,
+                        Quantity = 12,
+                        Category_Id = 3,
+                        Created_At = DateTime.Now,
+                        Updated_At = DateTime.Now,
+                    },
+                    new Product
+                    {
+                        Id = 6,
+                        Name = "Product 6",
+                        Price = 16,
+                        Quantity = 12,
+                        Category_Id = 1,
+                        Created_At = DateTime.Now,
+                        Updated_At = DateTime.Now,
+                    },
+                    new Product
+                    {
+                        Id = 7,
+                        Name = "Product 7",
+                        Price = 17,
+                        Quantity = 12,
+                        Category_Id = 2,
+                        Created_At = DateTime.Now,
+                        Updated_At = DateTime.Now,
+                    },
+                    new Product
+                    {
+                        Id = 8,
+                        Name = "Product 8",
+                        Price = 18,
+                        Quantity = 12,
+                        Category_Id = 2,
+                        Created_At = DateTime.Now,
+                        Updated_At = DateTime.Now,
+                    },
+                    new Product
+                    {
+                        Id = 9,
+                        Name = "Product 9",
+                        Price = 19,
+                        Quantity = 12,
+                        Category_Id = 1,
+                        Created_At = DateTime.Now,
+                        Updated_At = DateTime.Now,
+                    },
+                    new Product
+                    {
+                        Id = 10,
+                        Name = "Product 10",
+                        Price = 20,
+                        Quantity = 12,
+                        Category_Id = 3,
+                        Created_At = DateTime.Now,
+                        Updated_At = DateTime.Now,
+                    }
+                );
+            modelBuilder.Entity<Order>()
+                .HasData(
+                    new Order
+                    {
+                        Id = 1,
+                        Customer_Id = 1,
+                        Order_Date = new DateTime(2024, 02, 21),
+                        Delivery_Date = new DateTime(2024, 03, 16),
+                        Status = 1,
+                    },
+                    new Order
+                    {
+                        Id = 2,
+                        Customer_Id = 2,
+                        Order_Date = new DateTime(2024, 03, 01),
+                        Delivery_Date = new DateTime(2024, 03, 31),
+                        Status = 2,
+                    },
+                    new Order
+                    {
+                        Id = 3,
+                        Customer_Id = 2,
+                        Order_Date = new DateTime(2024, 04, 29),
+                        Delivery_Date = new DateTime(2024, 05, 07),
+                        Status = 2,
+                    },
+                    new Order
+                    {
+                        Id = 4,
+                        Customer_Id = 5,
+                        Order_Date = new DateTime(2024, 04, 04),
+                        Status = 0,
+                    },
+                    new Order
+                    {
+                        Id = 5,
+                        Customer_Id = 4,
+                        Order_Date = new DateTime(2024, 03, 31),
+                        Status = 0,
+                    },
+                    new Order
+                    {
+                        Id = 6,
+                        Customer_Id = 3,
+                        Order_Date = new DateTime(2023, 09, 23),
+                        Delivery_Date = new DateTime(2023, 10, 04),
+                        Status = 1,
+                    }
+                );
+            modelBuilder.Entity<OrderItem>()
+                .HasData(
+                    new OrderItem
+                    {
+                        Order_Id = 1,
+                        Product_Id = 1,
+                        Price = 11,
+                        Quantity = 1
+                    },
+                    new OrderItem
+                    {
+                        Order_Id = 2,
+                        Product_Id = 1,
+                        Price = 11,
+                        Quantity = 1
+                    },
+                    new OrderItem
+                    {
+                        Order_Id = 2,
+                        Product_Id = 2,
+                        Price = 12,
+                        Quantity = 1
+                    },
+                    new OrderItem
+                    {
+                        Order_Id = 3,
+                        Product_Id = 4,
+                        Price = 14,
+                        Quantity = 1
+                    },
+                    new OrderItem
+                    {
+                        Order_Id = 4,
+                        Product_Id = 1,
+                        Price = 11,
+                        Quantity = 1
+                    },
+                    new OrderItem
+                    {
+                        Order_Id = 4,
+                        Product_Id = 2,
+                        Price = 12,
+                        Quantity = 1
+                    },
+                    new OrderItem
+                    {
+                        Order_Id = 4,
+                        Product_Id = 4,
+                        Price = 14,
+                        Quantity = 1
+                    },
+                    new OrderItem
+                    {
+                        Order_Id = 5,
+                        Product_Id = 3,
+                        Price = 13,
+                        Quantity = 1
+                    },
+                    new OrderItem
+                    {
+                        Order_Id = 6,
+                        Product_Id = 1,
+                        Price = 11,
+                        Quantity = 1
+                    },
+                    new OrderItem
+                    {
+                        Order_Id = 6,
+                        Product_Id = 10,
+                        Price = 20,
+                        Quantity = 1
+                    }
+                );
         }
 
     }
--- 2World/Migrations/20240714133753_InitialDatabase.cs
+++ 2World/Migrations/20240714133753_InitialDatabase.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
 namespace _2World.Migrations
 {
     /// <inheritdoc />
@@ -45,22 +47,6 @@ namespace _2World.Migrations
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Customer", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Permission",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Created_At = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    Updated_At = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    Deleted_At = table.Column<DateTime>(type: "datetime2", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Permission", x => x.Id);
                 });
 
             migrationBuilder.CreateTable(
@@ -129,30 +115,6 @@ namespace _2World.Migrations
                 });
 
             migrationBuilder.CreateTable(
-                name: "RolePermissions",
-                columns: table => new
-                {
-                    Role_Id = table.Column<int>(type: "int", nullable: false),
-                    Permission_Id = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_RolePermissions", x => new { x.Role_Id, x.Permission_Id });
-                    table.ForeignKey(
-                        name: "FK_RolePermissions_Permission_Permission_Id",
-                        column: x => x.Permission_Id,
-                        principalTable: "Permission",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_RolePermissions_Role_Role_Id",
-                        column: x => x.Role_Id,
-                        principalTable: "Role",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
-            migrationBuilder.CreateTable(
                 name: "User",
                 columns: table => new
                 {
@@ -182,16 +144,14 @@ namespace _2World.Migrations
                 name: "OrderItem",
                 columns: table => new
                 {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
                     Order_Id = table.Column<int>(type: "int", nullable: false),
                     Product_Id = table.Column<int>(type: "int", nullable: false),
                     Quantity = table.Column<int>(type: "int", nullable: false),
-                    Price = table.Column<decimal>(type: "decimal(18,0)", nullable: false)
+                    Price = table.Column<decimal>(type: "decimal(18,0)", nullable: false),
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_OrderItem", x => x.Id);
+                    table.PrimaryKey("PK_OrderItem", x => new { x.Order_Id, x.Product_Id });
                     table.ForeignKey(
                         name: "FK_OrderItem_Order_Order_Id",
                         column: x => x.Order_Id,
@@ -206,15 +166,94 @@ namespace _2World.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "Id", "Created_At", "Deleted_At", "Name", "Updated_At" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4010), null, "Books", new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4011) },
+                    { 2, new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4014), null, "Electronics", new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4014) },
+                    { 3, new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4015), null, "Clothes", new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4016) }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Customer",
+                columns: new[] { "Id", "Address", "Created_At", "Deleted_At", "Email", "Name", "Password", "Phone", "Updated_At" },
+                values: new object[,]
+                {
+                    { 1, "Kozeyview", new DateTime(2024, 7, 14, 20, 37, 51, 351, DateTimeKind.Local).AddTicks(6342), null, "Imelda@gmail.com", "Ms. Colby Rosenbaum", "$2a$13$sH9ro5N6GSlZyq9HcQ5WXe2Hz87tt0KTAwUx1M/E2KBwYUl.xwMg2", "1-320-979-3192 x71343", new DateTime(2024, 7, 14, 20, 37, 51, 351, DateTimeKind.Local).AddTicks(6348) },
+                    { 2, "Briachester", new DateTime(2024, 7, 14, 20, 37, 51, 871, DateTimeKind.Local).AddTicks(6449), null, "Damian@gmail.com", "Miss Sydnee Ahmed Treutel", "$2a$13$jqRkkSrOG82i.eXkzPfJOuOsiipBjsQO79OQSuONfIaAdIhBGDwPi", "(281)594-1910", new DateTime(2024, 7, 14, 20, 37, 51, 871, DateTimeKind.Local).AddTicks(6455) },
+                    { 3, "New Harrison", new DateTime(2024, 7, 14, 20, 37, 52, 381, DateTimeKind.Local).AddTicks(2771), null, "Dina@gmail.com", "Miss Dorothea Eloy Gerlach", "$2a$13$ypxDNHDLV9s/PlZKEbspw.fdWSe2l/PrpyFyTZ0ndNkuJfdYcDy12", "(183)802-0356", new DateTime(2024, 7, 14, 20, 37, 52, 381, DateTimeKind.Local).AddTicks(2775) },
+                    { 4, "Laurineland", new DateTime(2024, 7, 14, 20, 37, 52, 911, DateTimeKind.Local).AddTicks(7048), null, "Kailyn@gmail.com", "Mrs. Violette Caesar Murazik", "$2a$13$19.jlTr/sr2EbBFRUch6hONz.6moMEzCmfqiYBP1Oy0ZL./Y0.HqC", "309-835-5595 x7631", new DateTime(2024, 7, 14, 20, 37, 52, 911, DateTimeKind.Local).AddTicks(7053) },
+                    { 5, "Port Chelsie", new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(3633), null, "Rowland@gmail.com", "Rosella Bradtke", "$2a$13$pyG3U.6UhiUcdnpHk71uBu2lvIpKtWu9kv/UgglUtVDHayvehzed2", "(504)568-7113 x59651", new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(3638) }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Role",
+                columns: new[] { "Id", "Created_At", "Deleted_At", "Name", "Updated_At" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 7, 14, 20, 37, 50, 319, DateTimeKind.Local).AddTicks(3208), null, "ADMIN", new DateTime(2024, 7, 14, 20, 37, 50, 319, DateTimeKind.Local).AddTicks(3219) },
+                    { 2, new DateTime(2024, 7, 14, 20, 37, 50, 319, DateTimeKind.Local).AddTicks(3221), null, "USER", new DateTime(2024, 7, 14, 20, 37, 50, 319, DateTimeKind.Local).AddTicks(3221) },
+                    { 3, new DateTime(2024, 7, 14, 20, 37, 50, 319, DateTimeKind.Local).AddTicks(3222), null, "GUEST", new DateTime(2024, 7, 14, 20, 37, 50, 319, DateTimeKind.Local).AddTicks(3223) }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Order",
+                columns: new[] { "Id", "Customer_Id", "Delivery_Date", "Order_Date", "Status" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2024, 3, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 2, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), 1 },
+                    { 2, 2, new DateTime(2024, 3, 31, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 3, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 2 },
+                    { 3, 2, new DateTime(2024, 5, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 4, 29, 0, 0, 0, 0, DateTimeKind.Unspecified), 2 },
+                    { 4, 5, null, new DateTime(2024, 4, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), 0 },
+                    { 5, 4, null, new DateTime(2024, 3, 31, 0, 0, 0, 0, DateTimeKind.Unspecified), 0 },
+                    { 6, 3, new DateTime(2023, 10, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 9, 23, 0, 0, 0, 0, DateTimeKind.Unspecified), 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Product",
+                columns: new[] { "Id", "Category_Id", "Created_At", "Deleted_At", "Description", "Image_Path", "Name", "Price", "Quantity", "Updated_At" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4079), null, null, null, "Product 1", 10m, 12, new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4080) },
+                    { 2, 2, new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4082), null, null, null, "Product 2", 12m, 12, new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4082) },
+                    { 3, 1, new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4084), null, null, null, "Product 3", 13m, 12, new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4084) },
+                    { 4, 3, new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4085), null, null, null, "Product 4", 14m, 12, new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4086) },
+                    { 5, 3, new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4087), null, null, null, "Product 5", 15m, 12, new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4087) },
+                    { 6, 1, new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4089), null, null, null, "Product 6", 16m, 12, new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4089) },
+                    { 7, 2, new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4090), null, null, null, "Product 7", 17m, 12, new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4090) },
+                    { 8, 2, new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4092), null, null, null, "Product 8", 18m, 12, new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4092) },
+                    { 9, 1, new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4093), null, null, null, "Product 9", 19m, 12, new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4094) },
+                    { 10, 3, new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4095), null, null, null, "Product 10", 20m, 12, new DateTime(2024, 7, 14, 20, 37, 53, 435, DateTimeKind.Local).AddTicks(4095) }
+                });
+
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new[] { "Id", "Created_At", "Deleted_At", "Email", "Name", "Password", "Phone", "Role_Id", "Updated_At" },
+                values: new object[] { 1, new DateTime(2024, 7, 14, 20, 37, 50, 828, DateTimeKind.Local).AddTicks(7782), null, "admin@gmail.com", "Admin", "$2a$13$15FZTW2OoL1nUWJC0h7df.Tp2X8e43pPZlO8dbOeDWe7vBG2Uvp8u", "0987654321", 1, new DateTime(2024, 7, 14, 20, 37, 50, 828, DateTimeKind.Local).AddTicks(7788) });
+
+            migrationBuilder.InsertData(
+                table: "OrderItem",
+                columns: new[] { "Order_Id", "Product_Id", "Price", "Quantity" },
+                values: new object[,]
+                {
+                    { 1, 1, 11, 1 },
+                    { 2, 1, 11, 1 },
+                    { 2, 2, 12, 1 },
+                    { 3, 4, 14, 1 },
+                    { 4, 1, 11, 1 },
+                    { 4, 2, 12, 1 },
+                    { 4, 4, 14, 1 },
+                    { 5, 3, 13, 1 },
+                    { 6, 1, 11, 1 },
+                    { 6, 10, 20, 1 }
+                });
+
             migrationBuilder.CreateIndex(
                 name: "IX_Order_Customer_Id",
                 table: "Order",
                 column: "Customer_Id");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_OrderItem_Order_Id",
-                table: "OrderItem",
-                column: "Order_Id");
 
             migrationBuilder.CreateIndex(
                 name: "IX_OrderItem_Product_Id",
@@ -225,11 +264,6 @@ namespace _2World.Migrations
                 name: "IX_Product_Category_Id",
                 table: "Product",
                 column: "Category_Id");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_RolePermissions_Permission_Id",
-                table: "RolePermissions",
-                column: "Permission_Id");
 
             migrationBuilder.CreateIndex(
                 name: "IX_User_Role_Id",
@@ -244,9 +278,6 @@ namespace _2World.Migrations
                 name: "OrderItem");
 
             migrationBuilder.DropTable(
-                name: "RolePermissions");
-
-            migrationBuilder.DropTable(
                 name: "User");
 
             migrationBuilder.DropTable(
@@ -254,9 +285,6 @@ namespace _2World.Migrations
 
             migrationBuilder.DropTable(
                 name: "Product");
-
-            migrationBuilder.DropTable(
-                name: "Permission");
 
             migrationBuilder.DropTable(
                 name: "Role");
